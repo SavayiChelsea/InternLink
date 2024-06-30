dad9163f5819b918033c246ffd86fbed1c6a5893
Savayi <savayichelsea90@gmail.com>
2024-06-30 11:52:04 +0300

    Profile photo enabled

--- resources/views/Admin/home.blade.php
+++ resources/views/Admin/home.blade.php
@@ -1,9 +1,15 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl  dark:text-gray-200 leading-tight">
-            {{ __('Admin Dashboard') }}
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            Hello {{ Auth::user()->name }}!
         </h2>
     </x-slot>
+
+    <div class ="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 1g:px-8">
+            This is the admin page
+        </div>
+    </div>
 
 
 </x-app-layout>
